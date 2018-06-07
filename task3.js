3a77204a81bb020757598f6c1c06c4d0a0b30c96
m0sa1k <orlov.sereja@gmail.com>
2018-06-07 18:49:02 +0700

    Задание 3: Разделение строки

--- task3.js
+++ task3.js
@@ -1,51 +1,34 @@
 transformString(process.argv[2]);
 
 function transformString(str) {
-	let array1 = [];
-	let array2 = [];
-	let array3 = [];
-	let mainArray = [];
+	let vowelsString = '', consonantsString = '', numbersString = '';
+	let mainString = '';
 
 	for (let i = 0; i < str.length; i++) {
-		if (vowelsRange(str[i])) {
-			array1.push(str.charAt(i));
-		} else if (consonantsRange(str[i])) {
-			array2.push(str.charAt(i));
+
+		let k = str[i].toUpperCase();
+
+		if (k.charCodeAt() === 65 ||
+			k.charCodeAt() === 69 ||
+			k.charCodeAt() === 73 ||
+			k.charCodeAt() === 74 ||
+			k.charCodeAt() === 79 ||
+			k.charCodeAt() === 85 ||
+			k.charCodeAt() === 89) {
+			vowelsString += str.charAt(i);
+		} else if (k.charCodeAt() > 65 && k.charCodeAt() <= 90) {
+			consonantsString += str.charAt(i);
 		} else if (str.charCodeAt(i) >= 48 && str.charCodeAt(i) <= 57) {
-			array3.push(str.charAt(i));
+			numbersString += str.charAt(i);
 		}
 	}
 
-	mainArray.push(array1.join(''));
-	mainArray.push(array2.join(''));
-	mainArray.push(array3.join(''));
-	
-	console.log(mainArray.join(' '));
+	mainString = outputLine(vowelsString) + outputLine(consonantsString) + outputLine(numbersString);
+	process.stdout.write(mainString+'\n');
 }
 
-function vowelsRange(letter) {
-	if (letter.charCodeAt() === 65 ||
-		letter.charCodeAt() === 69 ||
-		letter.charCodeAt() === 73 ||
-		letter.charCodeAt() === 74 ||
-		letter.charCodeAt() === 79 ||
-		letter.charCodeAt() === 85 ||
-		letter.charCodeAt() === 89 ||
-		letter.charCodeAt() === 97 ||
-		letter.charCodeAt() === 101 ||
-		letter.charCodeAt() === 105 ||
-		letter.charCodeAt() === 106 ||
-		letter.charCodeAt() === 111 ||
-		letter.charCodeAt() === 117 ||
-		letter.charCodeAt() === 121) {
-		return true;
-	} else return false;
-}
-
-function consonantsRange(letter) {
-	if ( letter.charCodeAt() > 65 && letter.charCodeAt() <= 90 || 
-		 letter.charCodeAt() > 97 && letter.charCodeAt() <= 122 && 
-		 !vowelsRange(letter)) {
-		return true;
-	} else return false;
+function outputLine(str) {
+	if (str != '') {
+		return str += ' ';
+	} else return str;
 }
